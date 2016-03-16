0d96bb4a003d9f0cd44f02307fde0d02d9115e3e
Nico Begic <nicolasbegic@alice.de>
2016-03-16 18:05:57 +0100

    [Added] Camera-Movement

---
+++ RTSGame/Assets/Scripts/CamZoom.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamZoom : MonoBehaviour 
+{
+    public int ZoomSpeed;
+	
+	void Update () 
+    {
+        transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * ZoomSpeed * Time.deltaTime);
+	}
+}
---
+++ RTSGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public int CamSpeed;
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * CamSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * CamSpeed * Time.deltaTime);
+	}
+}
